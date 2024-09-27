352fb1896755716f8c5834f430e996dc67cd0470
piyush gola <piyushgola45@gmail.com>
2024-09-27 22:05:41 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Dice-Game/', // Replace with your repository name
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  },
   plugins: [react()],
 })
